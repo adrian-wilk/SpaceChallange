2cebe7273161e33377095841aed06db0745dc38a
Adrian Wilk <awilk2@gmail.com>
2020-04-11 17:30:04 +0200

    phase-1.txt and phase-2.txt added. Item class added.

---
+++ src/Item.java
@@ -0,0 +1,11 @@
+public class Item {
+    private String name;
+    private int weight;
+
+
+    // Default Constructor
+    Item (String name, int weight){
+        this.name = name;
+        this.weight = weight;
+    }
+}
