d9a1758428abf219a53cbcc54e570d6c0d3855a0
Adrian Wilk <awilk2@gmail.com>
2020-04-11 19:08:32 +0200

    Item class modified. SpaceShip class added.

--- src/Item.java
+++ src/Item.java
@@ -8,4 +8,23 @@ public class Item {
         this.name = name;
         this.weight = weight;
     }
-}
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setWeight(int weight){
+        this.weight = weight;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+
+    }
+
+
